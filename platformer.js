b877925afe42ab5da8d039b25115de72bc16d389
RotaruDan <drotaru@ucm.es>
2014-04-07 21:15:46 +0200

    Added new TMX level, and new sprites: Mario, Bloopa and Goomba.

--- platformer.js
+++ platformer.js
@@ -5,444 +5,317 @@
 // as it loads a level json file.
 window.addEventListener("load",function() {
 
-// Set up an instance of the Quintus engine  and include
-// the Sprites, Scenes, Input and 2D module. The 2D module
-// includes the `TileLayer` class as well as the `2d` componet.
-var Q = window.Q = Quintus({audioSupported: [ 'wav','mp3','ogg' ]})
-        .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio")
-        // Maximize this game to whatever the size of the browser is
-        .setup({ maximize: true })
-        // And turn on default input controls and touch input (for UI)
-        .controls(true).touch()
-        // Enable sounds.
-        .enableSound();
+	// Set up an instance of the Quintus engine  and include
+	// the Sprites, Scenes, Input and 2D module. The 2D module
+	// includes the `TileLayer` class as well as the `2d` componet.
+	var Q = window.Q = Quintus({audioSupported: [ 'mp3','ogg' ]})
+	.include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio")
+	// Maximize this game to whatever the size of the browser is
+	.setup({ width: 320, height: 480 })
+	// And turn on default input controls and touch input (for UI)
+	.controls(true).touch()
+	// Enable sounds.
+	.enableSound();
 
-// Load and init audio files.
-
-
-Q.SPRITE_PLAYER = 1;
-Q.SPRITE_COLLECTABLE = 2;
-Q.SPRITE_ENEMY = 4;
-Q.SPRITE_DOOR = 8;
-Q.Sprite.extend("Player",{
-
-  init: function(p) {
-
-    this._super(p, {
-      sheet: "player",  // Setting a sprite sheet sets sprite width and height
-      sprite: "player",
-      direction: "right",
-      standingPoints: [ [ -16, 44], [ -23, 35 ], [-23,-48], [23,-48], [23, 35 ], [ 16, 44 ]],
-      duckingPoints : [ [ -16, 44], [ -23, 35 ], [-23,-10], [23,-10], [23, 35 ], [ 16, 44 ]],
-      jumpSpeed: -400,
-      speed: 300,
-      strength: 100,
-      score: 0,
-      type: Q.SPRITE_PLAYER,
-      collisionMask: Q.SPRITE_DEFAULT | Q.SPRITE_DOOR | Q.SPRITE_COLLECTABLE
-    });
-
-    this.p.points = this.p.standingPoints;
-
-    this.add('2d, platformerControls, animation, tween');
-
-    this.on("bump.top","breakTile");
-
-    this.on("sensor.tile","checkLadder");
-    this.on("enemy.hit","enemyHit");
-    this.on("jump");
-    this.on("jumped");
-
-    Q.input.on("down",this,"checkDoor");
-  },
-
-  jump: function(obj) {
-    // Only play sound once.
-    if (!obj.p.playedJump) {
-      Q.audio.play('jump.mp3');
-      obj.p.playedJump = true;
-    }
-  },
-
-  jumped: function(obj) {
-    obj.p.playedJump = false;
-  },
-
-  checkLadder: function(colObj) {
-    if(colObj.p.ladder) { 
-      this.p.onLadder = true;
-      this.p.ladderX = colObj.p.x;
-    }
-  },
-
-  checkDoor: function() {
-    this.p.checkDoor = true;
-  },
-
-  resetLevel: function() {
-    Q.stageScene("level1");
-    this.p.strength = 100;
-    this.animate({opacity: 1});
-    Q.stageScene('hud', 3, this.p);
-  },
-
-  enemyHit: function(data) {
-    var col = data.col;
-    var enemy = data.enemy;
-    this.p.vy = -150;
-    if (col.normalX == 1) {
-      // Hit from left.
-      this.p.x -=15;
-      this.p.y -=15;
-    }
-    else {
-      // Hit from right;
-      this.p.x +=15;
-      this.p.y -=15;
-    }
-    this.p.immune = true;
-    this.p.immuneTimer = 0;
-    this.p.immuneOpacity = 1;
-    this.p.strength -= 25;
-    Q.stageScene('hud', 3, this.p);
-    if (this.p.strength == 0) {
-      this.resetLevel();
-    }
-  },
-
-  continueOverSensor: function() {
-    this.p.vy = 0;
-    if(this.p.vx != 0) {
-      this.play("walk_" + this.p.direction);
-    } else {
-      this.play("stand_" + this.p.direction);
-    }
-  },
-
-  breakTile: function(col) {
-    if(col.obj.isA("TileLayer")) {
-      if(col.tile == 24) { col.obj.setTile(col.tileX,col.tileY, 36); }
-      else if(col.tile == 36) { col.obj.setTile(col.tileX,col.tileY, 24); }
-    }
-    Q.audio.play('coin.mp3');
-  },
-
-  step: function(dt) {
-    var processed = false;
-    if (this.p.immune) {
-      // Swing the sprite opacity between 50 and 100% percent when immune.
-      if ((this.p.immuneTimer % 12) == 0) {
-        var opacity = (this.p.immuneOpacity == 1 ? 0 : 1);
-        this.animate({"opacity":opacity}, 0);
-        this.p.immuneOpacity = opacity;
-      }
-      this.p.immuneTimer++;
-      if (this.p.immuneTimer > 144) {
-        // 3 seconds expired, remove immunity.
-        this.p.immune = false;
-        this.animate({"opacity": 1}, 1);
-      }
-    }
-
-    if(this.p.onLadder) {
-      this.p.gravity = 0;
-
-      if(Q.inputs['up']) {
-        this.p.vy = -this.p.speed;
-        this.p.x = this.p.ladderX;
-        this.play("climb");
-      } else if(Q.inputs['down']) {
-        this.p.vy = this.p.speed;
-        this.p.x = this.p.ladderX;
-        this.play("climb");
-      } else {
-        this.continueOverSensor();
-      }
-      processed = true;
-    } 
-      
-    if(!processed && this.p.door) {
-      this.p.gravity = 1;
-      if(this.p.checkDoor && this.p.landed > 0) {
-        // Enter door.
-        this.p.y = this.p.door.p.y;
-        this.p.x = this.p.door.p.x;
-        this.play('climb');
-        this.p.toDoor = this.p.door.findLinkedDoor();
-        processed = true;
-      }
-      else if (this.p.toDoor) {
-        // Transport to matching door.
-        this.p.y = this.p.toDoor.p.y;
-        this.p.x = this.p.toDoor.p.x;
-        this.stage.centerOn(this.p.x, this.p.y);
-        this.p.toDoor = false;
-        this.stage.follow(this);
-        processed = true;
-      }
-    } 
-      
-    if(!processed) { 
-      this.p.gravity = 1;
-
-      if(Q.inputs['down'] && !this.p.door) {
-        this.p.ignoreControls = true;
-        this.play("duck_" + this.p.direction);
-        if(this.p.landed > 0) {
-          this.p.vx = this.p.vx * (1 - dt*2);
-        }
-        this.p.points = this.p.duckingPoints;
-      } else {
-        this.p.ignoreControls = false;
-        this.p.points = this.p.standingPoints;
-
-        if(this.p.vx > 0) {
-          if(this.p.landed > 0) {
-            this.play("walk_right");
-          } else {
-            this.play("jump_right");
-          }
-          this.p.direction = "right";
-        } else if(this.p.vx < 0) {
-          if(this.p.landed > 0) {
-            this.play("walk_left");
-          } else {
-            this.play("jump_left");
-          }
-          this.p.direction = "left";
-        } else {
-          this.play("stand_" + this.p.direction);
-        }
-           
-      }
-    }
-
-    this.p.onLadder = false;
-    this.p.door = false;
-    this.p.checkDoor = false;
+	// Load and init audio files.
 
 
-    if(this.p.y > 1000) {
-      this.stage.unfollow();
-    }
+	Q.SPRITE_PLAYER = 1;
+	Q.SPRITE_COLLECTABLE = 2;
+	Q.SPRITE_ENEMY = 4;
+	Q.Sprite.extend("Player",{
 
-    if(this.p.y > 2000) {
-      this.resetLevel();
-    }
-  }
-});
+		init: function(p) {
 
-Q.Sprite.extend("Enemy", {
-  init: function(p,defaults) {
+			this._super(p, {
+				sheet: "mario_small",  // Setting a sprite sheet sets sprite width and height
+				sprite: "mario_small",
+				direction: "right",
+				// puntos dentro del cuadrado de mario
+				standingPoints: [ [ -16, 16], [ -16, -16 ], [16, -16], [16, 16] ],
+				//copia/pega
+				duckingPoints : [ [ -16, 16], [ -16, -16 ], [16, -16], [16, 16] ],
 
-    this._super(p,Q._defaults(defaults||{},{
-      sheet: p.sprite,
-      vx: 50,
-      defaultDirection: 'left',
-      type: Q.SPRITE_ENEMY,
-      collisionMask: Q.SPRITE_DEFAULT
-    }));
+				jumpSpeed: -400,
+				speed: 290,
+				strength: 100,
+				score: 0,
+				type: Q.SPRITE_PLAYER,
+				collisionMask: Q.SPRITE_DEFAULT | Q.SPRITE_COLLECTABLE
+			});
 
-    this.add("2d, aiBounce, animation");
-    this.on("bump.top",this,"die");
-    this.on("hit.sprite",this,"hit");
-  },
+			this.p.points = this.p.standingPoints;
 
-  step: function(dt) {
-    if(this.p.dead) {
-      this.del('2d, aiBounce');
-      this.p.deadTimer++;
-      if (this.p.deadTimer > 24) {
-        // Dead for 24 frames, remove it.
-        this.destroy();
-      }
-      return;
-    }
-    var p = this.p;
+			this.add('2d, platformerControls, animation, tween');
 
-    p.vx += p.ax * dt;
-    p.vy += p.ay * dt;
+			this.on("bump.top","breakTile");
 
-    p.x += p.vx * dt;
-    p.y += p.vy * dt;
+			this.on("enemy.hit","enemyHit");
+			this.on("jump");
+			this.on("jumped");
+		},
 
-    this.play('walk');
-  },
+		jump: function(obj) {
+			// Only play sound once.
+			if (!obj.p.playedJump) {
+				//Q.audio.play('jump.mp3');
+				obj.p.playedJump = true;
+			}
+		},
 
-  hit: function(col) {
-    if(col.obj.isA("Player") && !col.obj.p.immune && !this.p.dead) {
-      col.obj.trigger('enemy.hit', {"enemy":this,"col":col});
-      Q.audio.play('hit.mp3');
-    }
-  },
+		jumped: function(obj) {
+			obj.p.playedJump = false;
+		},
 
-  die: function(col) {
-    if(col.obj.isA("Player")) {
-      Q.audio.play('coin.mp3');
-      this.p.vx=this.p.vy=0;
-      this.play('dead');
-      this.p.dead = true;
-      var that = this;
-      col.obj.p.vy = -300;
-      this.p.deadTimer = 0;
-    }
-  }
-});
+		resetLevel: function() {
+			Q.stageScene("levelOK");
+			this.p.strength = 100;
+			this.animate({opacity: 1});
+			Q.stageScene('hud', 3, this.p);
+		},
 
-Q.Enemy.extend("Fly", {
+		enemyHit: function(data) {
+			var col = data.col;
+			var enemy = data.enemy;
+			this.p.vy = -150;
+			if (col.normalX == 1) {
+				// Hit from left.
+				this.p.x -=15;
+				this.p.y -=15;
+			}
+			else {
+				// Hit from right;
+				this.p.x +=15;
+				this.p.y -=15;
+			}
+			this.p.strength -= 25;
+			Q.stageScene('hud', 3, this.p);
+			if (this.p.strength == 0) {
+				this.resetLevel();
+			}
+		},
 
-});
+		continueOverSensor: function() {
+			this.p.vy = 0;
+			if(this.p.vx != 0) {
+				this.play("walk_" + this.p.direction);
+			} else {
+				this.play("stand_" + this.p.direction);
+			}
+		},
 
-Q.Enemy.extend("Slime", {
-  init: function(p) {
-    this._super(p,{
-      w: 55,
-      h: 34
-    });
-  }
-});
+		breakTile: function(col) {
+			if(col.obj.isA("TileLayer")) {
+				if(col.tile == 24) { col.obj.setTile(col.tileX,col.tileY, 36); }
+				else if(col.tile == 36) { col.obj.setTile(col.tileX,col.tileY, 24); }
+			}
+			////Q.audio.play('coin.mp3');
+		},
 
-Q.Enemy.extend("Snail", {
-  init: function(p) {
-    this._super(p,{
-      w: 55,
-      h: 36
-    });
-  }
+		step: function(dt) {
+			var processed = false;
 
-});
+			if(!processed) { 
+				this.p.gravity = 1;
 
-Q.Sprite.extend("Collectable", {
-  init: function(p) {
-    this._super(p,{
-      sheet: p.sprite,
-      type: Q.SPRITE_COLLECTABLE,
-      collisionMask: Q.SPRITE_PLAYER,
-      sensor: true,
-      vx: 0,
-      vy: 0,
-      gravity: 0
-    });
-    this.add("animation");
+				if(Q.inputs['down']) {
+					this.p.ignoreControls = true;
+					this.play("duck_" + this.p.direction);
+					if(this.p.landed > 0) {
+						this.p.vx = this.p.vx * (1 - dt*2);
+					}
+					this.p.points = this.p.duckingPoints;
+				} else {
+					this.p.ignoreControls = false;
+					this.p.points = this.p.standingPoints;
 
-    this.on("sensor");
-  },
+					if(this.p.vx > 0) {
+						if(this.p.landed > 0) {
+							this.play("walk_right");
+						} else {
+							this.play("jump_right");
+						}
+						this.p.direction = "right";
+					} else if(this.p.vx < 0) {
+						if(this.p.landed > 0) {
+							this.play("walk_left");
+						} else {
+							this.play("jump_left");
+						}
+						this.p.direction = "left";
+					} else {
+						this.play("stand_" + this.p.direction);
+					}
 
-  // When a Collectable is hit.
-  sensor: function(colObj) {
-    // Increment the score.
-    if (this.p.amount) {
-      colObj.p.score += this.p.amount;
-      Q.stageScene('hud', 3, colObj.p);
-    }
-    Q.audio.play('coin.mp3');
-    this.destroy();
-  }
-});
+				}
+			}
 
-Q.Sprite.extend("Door", {
-  init: function(p) {
-    this._super(p,{
-      sheet: p.sprite,
-      type: Q.SPRITE_DOOR,
-      collisionMask: Q.SPRITE_NONE,
-      sensor: true,
-      vx: 0,
-      vy: 0,
-      gravity: 0
-    });
-    this.add("animation");
 
-    this.on("sensor");
-  },
-  findLinkedDoor: function() {
-    return this.stage.find(this.p.link);
-  },
-  // When the player is in the door.
-  sensor: function(colObj) {
-    // Mark the door object on the player.
-    colObj.p.door = this;
-  }
-});
 
-Q.Collectable.extend("Heart", {
-  // When a Heart is hit.
-  sensor: function(colObj) {
-    // Increment the strength.
-    if (this.p.amount) {
-      colObj.p.strength = Math.max(colObj.p.strength + 25, 100);
-      Q.stageScene('hud', 3, colObj.p);
-      Q.audio.play('heart.mp3');
-    }
-    this.destroy();
-  }
-});
+			if(this.p.y > 1000) {
+				this.stage.unfollow();
+			}
 
-Q.scene("level1",function(stage) {
-  Q.stageTMX("level1.tmx",stage);
+			if(this.p.y > 2000) {
+				this.resetLevel();
+			}
+		}
+	});
 
-  stage.add("viewport").follow(Q("Player").first());
-});
+	Q.Sprite.extend("Enemy", {
+		init: function(p,defaults) {
 
-Q.scene('hud',function(stage) {
-  var container = stage.insert(new Q.UI.Container({
-    x: 50, y: 0
-  }));
+			this._super(p,Q._defaults(defaults||{},{
+				sheet: p.sprite,
+				vx: 50,
+				defaultDirection: 'left',
+				type: Q.SPRITE_ENEMY,
+				collisionMask: Q.SPRITE_DEFAULT
+			}));
 
-  var label = container.insert(new Q.UI.Text({x:200, y: 20,
-    label: "Score: " + stage.options.score, color: "white" }));
+			this.add("2d, aiBounce, animation");
+			this.on("bump.top",this,"die");
+			this.on("hit.sprite",this,"hit");
+		},
 
-  var strength = container.insert(new Q.UI.Text({x:50, y: 20,
-    label: "Health: " + stage.options.strength + '%', color: "white" }));
+		step: function(dt) {
+			if(this.p.dead) {
+				this.del('2d, aiBounce');
+				this.p.deadTimer++;
+				if (this.p.deadTimer > 24) {
+					// Dead for 24 frames, remove it.
+					this.destroy();
+				}
+				return;
+			}
+			var p = this.p;
 
-  container.fit(20);
-});
+			p.vx += p.ax * dt;
+			p.vy += p.ay * dt;
 
-Q.loadTMX("level1.tmx, collectables.json, doors.json, enemies.json, fire.mp3, jump.mp3, heart.mp3, hit.mp3, coin.mp3, player.json, player.png", function() {
-    Q.compileSheets("player.png","player.json");
-    Q.compileSheets("collectables.png","collectables.json");
-    Q.compileSheets("enemies.png","enemies.json");
-    Q.compileSheets("doors.png","doors.json");
-    Q.animations("player", {
-      walk_right: { frames: [0,1,2,3,4,5,6,7,8,9,10], rate: 1/15, flip: false, loop: true },
-      walk_left: { frames:  [0,1,2,3,4,5,6,7,8,9,10], rate: 1/15, flip:"x", loop: true },
-      jump_right: { frames: [13], rate: 1/10, flip: false },
-      jump_left: { frames:  [13], rate: 1/10, flip: "x" },
-      stand_right: { frames:[14], rate: 1/10, flip: false },
-      stand_left: { frames: [14], rate: 1/10, flip:"x" },
-      duck_right: { frames: [15], rate: 1/10, flip: false },
-      duck_left: { frames:  [15], rate: 1/10, flip: "x" },
-      climb: { frames:  [16, 17], rate: 1/3, flip: false }
-    });
-    var EnemyAnimations = {
-      walk: { frames: [0,1], rate: 1/3, loop: true },
-      dead: { frames: [2], rate: 1/10 }
-    };
-    Q.animations("fly", EnemyAnimations);
-    Q.animations("slime", EnemyAnimations);
-    Q.animations("snail", EnemyAnimations);
-    Q.stageScene("level1");
-    Q.stageScene('hud', 3, Q('Player').first().p);
-  
-}, {
-  progressCallback: function(loaded,total) {
-    var element = document.getElementById("loading_progress");
-    element.style.width = Math.floor(loaded/total*100) + "%";
-    if (loaded == total) {
-      document.getElementById("loading").remove();
-    }
-  }
-});
+			p.x += p.vx * dt;
+			p.y += p.vy * dt;
 
-// ## Possible Experimentations:
-// 
-// The are lots of things to try out here.
-// 
-// 1. Modify level.json to change the level around and add in some more enemies.
-// 2. Add in a second level by creating a level2.json and a level2 scene that gets
-//    loaded after level 1 is complete.
-// 3. Add in a title screen
-// 4. Add in a hud and points for jumping on enemies.
-// 5. Add in a `Repeater` behind the TileLayer to create a paralax scrolling effect.
+			this.play('walk');
+		},
 
+		hit: function(col) {
+			if(col.obj.isA("Player") && !this.p.dead) {
+				col.obj.trigger('enemy.hit', {"enemy":this,"col":col});
+				////Q.audio.play('hit.mp3');
+			}
+		},
+
+		die: function(col) {
+			if(col.obj.isA("Player")) {
+				//Q.audio.play('coin.mp3');
+				this.p.vx=this.p.vy=0;
+				this.play('dead');
+				this.p.dead = true;
+				var that = this;
+				col.obj.p.vy = -300;
+				this.p.deadTimer = 0;
+			}
+		}
+	});
+
+	Q.Enemy.extend("Bloopa", {
+		init: function(p) {
+			this._super(p,{
+				sheet: "bloopa",  // Setting a sprite sheet sets sprite width and height
+				sprite: "bloopa"
+			});
+		}
+	});
+	
+	Q.Enemy.extend("Goomba", {
+		init: function(p) {
+			this._super(p,{
+				sheet: "goomba",  // Setting a sprite sheet sets sprite width and height
+				sprite: "goomba"
+			});
+		}
+	});
+
+	Q.Sprite.extend("Collectable", {
+		init: function(p) {
+			this._super(p,{
+				sheet: p.sprite,
+				type: Q.SPRITE_COLLECTABLE,
+				collisionMask: Q.SPRITE_PLAYER,
+				sensor: true,
+				vx: 0,
+				vy: 0,
+				gravity: 0
+			});
+			this.add("animation");
+
+			this.on("sensor");
+		},
+
+		// When a Collectable is hit.
+		sensor: function(colObj) {
+			// Increment the score.
+			if (this.p.amount) {
+				colObj.p.score += this.p.amount;
+				Q.stageScene('hud', 3, colObj.p);
+			}
+			//Q.audio.play('coin.mp3');
+			this.destroy();
+		}
+	});
+
+	Q.scene("levelOK",function(stage) {
+		Q.stageTMX("levelOK.tmx",stage);
+
+		stage.add("viewport").follow(Q("Player").first(), { x:true, y:false });
+		stage.centerOn(160, 372);
+	});
+
+	Q.scene('hud',function(stage) {
+		var container = stage.insert(new Q.UI.Container({
+			x: 20, y: 0
+		}));
+
+		var label = container.insert(new Q.UI.Text({x:200, y: 20,
+													label: "Puntos: " + stage.options.score, color: "white" }));
+
+		var strength = container.insert(new Q.UI.Text({x:50, y: 20,
+													   label: "Vida: " + stage.options.strength + '%', color: "white" }));
+
+		container.fit(20);
+	});
+
+	Q.loadTMX("levelOK.tmx, mario_small.json, mario_small.png, bloopa.json, bloopa.png, goomba.json, goomba.png", function() {
+		Q.compileSheets("mario_small.png","mario_small.json");
+		Q.compileSheets("bloopa.png","bloopa.json");
+		Q.compileSheets("goomba.png","goomba.json");
+		Q.animations("mario_small", {
+			walk_right: { frames: [0,1,2], rate: 1/12, flip: false, loop: true },
+			walk_left: { frames:  [0,1,2], rate: 1/12, flip:"x", loop: true },
+			jump_right: { frames: [4], rate: 1/10, flip: false },
+			jump_left: { frames:  [4], rate: 1/10, flip: "x" },
+			stand_right: { frames:[0], rate: 1/10, flip: false },
+			stand_left: { frames: [0], rate: 1/10, flip:"x" },
+			duck_right: { frames: [6], rate: 1/10, flip: false },
+			duck_left: { frames:  [6], rate: 1/10, flip: "x" }
+		});
+		var EnemyAnimations = {
+			walk: { frames: [0,1], rate: 1/3, loop: true },
+			dead: { frames: [2], rate: 1/10 }
+		};
+		Q.animations("bloopa", EnemyAnimations);
+		Q.animations("goomba", EnemyAnimations);
+		Q.stageScene("levelOK");
+		Q.stageScene('hud', 3, Q('Player').first().p);
+
+	}, {
+		progressCallback: function(loaded,total) {
+			var element = document.getElementById("loading_progress");
+			element.style.width = Math.floor(loaded/total*100) + "%";
+			if (loaded == total) {
+				document.getElementById("loading").remove();
+			}
+		}
+	});
 });
